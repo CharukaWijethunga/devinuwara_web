ddfbc6169c69e3d5c36cf627f93dc45d8c489a7d
Charuka Wijethunga <charukamwijethunga@gmail.com>
2023-05-19 19:30:02 +0530

    update the carousel and add translations

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n/config';
-import Title from "./Title";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const title = ReactDOM.createRoot(document.getElementById('title'));
+
 root.render(<App />);
-title.render(<Title/>);
+
 
