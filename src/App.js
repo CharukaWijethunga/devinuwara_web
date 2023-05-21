686b6a6f17a26d7b0a4ca2d281ff9a1881f53ff1
Charitha Wijetunga <Charitha@bileetasl.bileeta.com>
2023-05-21 10:54:15 +0530

    home add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
+import HomeExp from "./Pages/home_example";
 import Donation from "./Pages/Donation";
 import News from "./Pages/News";
 import Rituals from "./Pages/Rituals";
@@ -11,7 +12,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<HomeExp />} />
           <Route path="/rituals" element={<Rituals />} />
           <Route path="/news" element={<News />} />
           <Route path="/donation" element={<Donation />} />
